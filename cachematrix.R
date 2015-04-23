f61d4cf8b7e3fb592e1aea67552afffa2e2e0035
ngwells <nathangwells@gmail.com>
2015-04-23 13:20:44 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Thia function will create several operations to utilize
+## including: set, get, getsolve, setsolve
 
+
+makeCacheMatrix<- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) m <<- solve
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
+
+##this function will check to see if the result is already cached in memory.
+## If it is - then it doesn't have t repeat all the calcs - esp for large matrices.
+
+
+cacheSolve<- function(x, ...) {
+        m <- x$getsolve()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setsolve(m)          ## Return a matrix that is the inverse of 'x'
+        m
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
